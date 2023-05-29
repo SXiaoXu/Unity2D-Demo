564c0758a769308e312cfc07403592a5c6e176ad
XXu <284898541@qq.com>
2023-05-29 19:55:03 +0800

    Use UnityWebRequest to load avatar

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -4,45 +4,106 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TapTap.Bootstrap;
 using TapTap.Common;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
 public class Menu : MonoBehaviour
 {
-    public void PlayGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    public Text nickname;
+    public Image avatar;
 
+    public void Start()
+    {
+        SetName();
     }
-    public void QuitGame(){
+
+    public async void SetName()
+    {
+        var currentUser = await TDSUser.GetCurrent();
+
+        if (null != currentUser)
+        {
+            nickname.text = currentUser["nickname"].ToString(); // 昵称
+            string avatarPath = currentUser["avatar"].ToString(); // 头像
+            Debug.Log("头像的 URL 是：" + avatarPath);
+            //加载头像
+            StartCoroutine(GetTexFromUnityWebRequest(avatarPath));
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
         Application.Quit();
     }
-    public  async void Logout(){
-         var currentUser = await TDSUser.GetCurrent();
+
+    public async void Logout()
+    {
+        var currentUser = await TDSUser.GetCurrent();
 
         if (null == currentUser)
         {
-        Debug.Log("未登录");
-        }else{
-            await TDSUser.Logout();
-             Debug.Log("已退出登录");
-             //退出登录后返回登录页面：
-             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+            Debug.Log("未登录");
         }
-
+        else
+        {
+            await TDSUser.Logout();
+            Debug.Log("已退出登录");
+            //退出登录后返回登录页面：
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
     }
 
     //打开公告
-    public void openBillboard(){
+    public void openBillboard()
+    {
         Debug.Log("打开公告");
     }
+
     //打开内嵌动态
-    public void openForum(){
+    public void openForum()
+    {
         Debug.Log("打开动态");
     }
+
     //跳转成就
-    public void openAchievement(){
-       Debug.Log("打开成就");
+    public void openAchievement()
+    {
+        Debug.Log("打开成就");
     }
+
     //跳转排行榜
-    public void openLeaderboard(){
+    public void openLeaderboard()
+    {
         Debug.Log("打开排行榜");
     }
-   
+
+    //加载头像
+    IEnumerator GetTexFromUnityWebRequest(string imageUrl)
+    {
+        using var request = UnityWebRequest.Get(imageUrl);
+        yield return request.SendWebRequest();
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(request.result);
+            Debug.Log(request.error);
+        }
+        else
+        {
+            var texture = new Texture2D(150, 150);
+            texture.LoadImage(request.downloadHandler.data);
+            var sprite = Sprite.Create(
+                texture,
+                new Rect(80, 80, 160, 160),
+                new Vector2(0.5f, 0.5f)
+            );
+            avatar.sprite = sprite;
+            avatar.SetNativeSize();
+            Resources.UnloadUnusedAssets();
+        }
+    }
 }
